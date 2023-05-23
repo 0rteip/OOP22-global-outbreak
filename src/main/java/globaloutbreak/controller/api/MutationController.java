7ce2d8ac443b296b37b96360aecf0ab71612c381
Giacomo <giacomo.severi5@studio.unibo.it>
2023-05-23 10:06:12 +0200

    add Mutation controller

---
+++ src/main/java/globaloutbreak/controller/api/MutationController.java
@@ -0,0 +1,16 @@
+package globaloutbreak.controller.api;
+
+
+public interface MutationController {
+
+
+    /**
+     * load mutation 
+     * @param path name of the mutation
+     */
+    public void loadMutationFromFile(final String path);
+    
+
+
+  
+}
---
+++ src/main/java/globaloutbreak/controller/impl/MutationControllerImpl.java
@@ -0,0 +1,54 @@
+package globaloutbreak.controller.impl;
+
+import java.io.IOException;
+import java.util.List;
+
+import globaloutbreak.controller.api.MutationController;
+import globaloutbreak.model.api.Mutation;
+import globaloutbreak.model.api.MutationFactory;
+import globaloutbreak.model.impl.MutationData;
+import globaloutbreak.model.impl.MutationFactoryImpl;
+
+public class MutationControllerImpl implements MutationController{
+
+    private MutationData mutationData;
+
+    public MutationControllerImpl(){
+
+        MutationFactoryImpl factory = new MutationFactoryImpl();
+        this.mutationData= new MutationData(factory);
+
+    }
+
+
+
+    @Override
+    public void loadMutationFromFile(final String name) {
+        //-chiamata qwuando uno schiaccia su un potenziamento
+        //leggo dati completi da file
+        //aggiorno la view
+
+
+        {
+            try {
+                 mutationData.loadMutationFromJson(name);
+                
+                List<Mutation> mutations = mutationData.getMutations();
+    
+                // La lista dei potenziamenti è pronta per essere utilizzata
+                // Esempio: visualizza i nomi dei potenziamenti
+                for (Mutation mutation : mutations) {
+                    System.out.println(mutation.getName());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } 
+        }
+    }
+
+
+
+
+   
+    
+}
