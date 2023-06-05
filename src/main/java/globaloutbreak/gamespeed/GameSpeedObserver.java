688b1e1eff8d6f2bafc7eb1dea78d7bb6970d6df
Pietro Ventrucci <pietro.ventrucci@studio.unibo.it>
2023-06-05 17:48:33 +0200

    Added GameSpeed with file configuration

---
+++ src/main/java/globaloutbreak/gamespeed/GameSpeedObserver.java
@@ -0,0 +1,30 @@
+package globaloutbreak.gamespeed;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import globaloutbreak.view.View;
+
+/**
+ * Game Speed observer.
+ */
+public final class GameSpeedObserver implements PropertyChangeListener {
+
+    private final View view;
+
+    /**
+     * Create an observer.
+     * 
+     * @param view
+     *             the main view
+     */
+    public GameSpeedObserver(final View view) {
+        this.view = view.clone();
+    }
+
+    @Override
+    public void propertyChange(final PropertyChangeEvent arg0) {
+        this.view.setGameSpeed((GameSpeed) arg0.getNewValue());
+    }
+
+}
