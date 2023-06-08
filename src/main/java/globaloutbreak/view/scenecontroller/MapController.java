1c63b490752d670ae3f194703ef6d43d8c57ddd0
Luca Carabini <luca.carabini@studio.unibo.it>
2023-06-08 14:39:27 +0200

    Create class MapController.java

---
+++ src/main/java/globaloutbreak/view/scenecontroller/MapController.java
@@ -0,0 +1,74 @@
+package globaloutbreak.view.scenecontroller;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+
+public final class MapController extends AbstractSceneController implements SceneInitializer{
+    @FXML
+    private StackPane mapPane;
+
+    @FXML
+    private Button playPausaBut;
+
+    @FXML
+    private Label mapLab;
+
+    @FXML
+    private Button settingBut;
+
+    @FXML
+    private Button mutationBut;
+
+    @FXML
+    private TextField infectedText;
+
+    @FXML
+    private TextField regionText;
+
+    @FXML
+    private TextField deathText;
+
+    @FXML
+    private Button worldBut;
+
+
+    @FXML
+    public final void selectRegion1(MouseEvent e) {
+
+    }
+
+    @FXML
+    public final void selectRegion(MouseEvent e) {
+
+    }
+
+    @FXML
+    public final void openSettings(MouseEvent e) {
+
+    }
+
+    @FXML
+    public final void goToGeneralGraph(MouseEvent e) {
+
+    }
+
+    @FXML
+    public final void goToMutation(MouseEvent e) {
+
+    }
+    
+    @FXML
+    public final void startStop(MouseEvent e) {
+
+    }
+
+    @Override
+    public void initializeScene() {
+        
+    }
+    
+    
+}
